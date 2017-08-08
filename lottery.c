62a98c49b5c1d4fe201e2c1d96164739fda026c9
Michal Odrobina <michalodrobina@gmail.com>
2017-08-08 09:53:28 +0100

    step 6 update

--- lottery.c
+++ lottery.c
@@ -36,8 +36,13 @@ int main(int argc, char** argv)
     }
 
     int result = calculate_result(white_balls, power_ball);
+ 
+    if (7 == power_ball)
+    {
+        result = result * 2;
+    }
 
-    printf("%d percent chance of winning\n", result);
-
-    return 0;
+     printf("%d percent chance of winning\n", result);
+ 
+     return 0;
 }
